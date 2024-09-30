0b54269ca1abf286506c8cb3c85d192dee390017
Vikram Sethu Madhavan <vikramsethumadhavan@Vikrams-Mac-Studio.local>
2024-09-30 19:17:04 +0530

    second

---
+++ app/error.js
@@ -0,0 +1,14 @@
+// app/error.js
+'use client';
+
+export default function GlobalError({ error, reset }) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <p>{error.message}</p>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
--- app/error/page.js
+++
@@ -1,6 +0,0 @@
-// app/error/page.js
-export default function ErrorPage() {
-  throw new Error("Simulated 500 Error");
-
-  return <div>Should not reach here</div>;
-}
